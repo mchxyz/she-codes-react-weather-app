ea264214b0520036af8012e39ae310b10d75c244
MCH <111715559+mchxyz@users.noreply.github.com>
2024-08-14 19:08:53 -0400

    Added GitHub Repo URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,25 @@ import React from 'react';
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className='App'>
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by <a 
+        href='https://github.com/mchxyz' 
+        target='_blank' 
+        rel='noreferrer'
+        >MA
+        </a>{" "}
+        and is{" "}
+        <a 
+        href='https://github.com/mchxyz/she-codes-react-weather-app' 
+        target='_blank'
+        rel='noreferrer'
+        > 
+        open-sourced on GitHub
+        </a>
+    </footer>
+  </div>
+  );
 }
