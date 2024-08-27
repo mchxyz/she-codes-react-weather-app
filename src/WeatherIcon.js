a0960c80686c3929b0c8211f7723a40b5ed90737
MCH <111715559+mchxyz@users.noreply.github.com>
2024-08-27 00:37:44 -0400

    Forecast Layout

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -3,7 +3,7 @@ import ReactAnimatedWeather from 'react-animated-weather';
 
 export default function WeatherIcon(props) {
       const codeMapping = {
-        "O1d": "CLEAR_DAY",
+        "01d": "CLEAR_DAY",
         "01n": "CLEAR_NIGHT",
         "02d": "PARTLY_CLOUDY_DAY",
         "02n": "PARTLY_CLOUDY_NIGHT",
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather 
        icon={codeMapping[props.code]}
        color="#1e1e1e"
-       size={52}
+       size={props.size}
        animate={true}
      />
   );
